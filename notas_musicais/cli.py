c1344c228d930b19792c2b344f9dabb8bb38533b
johonnes <wjohonnes@gmail.com>
2024-09-12 03:07:16 -0300

    atualização do CLI e teste

--- notas_musicais/cli.py
+++ notas_musicais/cli.py
@@ -3,16 +3,33 @@ from rich.console import Console
 from rich.table import Table
 from typer import Argument, Typer
 
-from notas_musicais.escala import escala
+from notas_musicais.acordes import acorde as _acorde
+from notas_musicais.escala import escala as _escala
 
 console = Console()
 app = Typer()
 
 
 @app.command()
-def escalas(tonica=Argument('c'), tonalidade=Argument('maior')):
+def escala(
+    tonica: str = Argument('c', help='Tônica da escala'),
+    tonalidade: str = Argument('maior', help='Tonalidade da escala'),
+):
     table = Table()
-    notas, graus = escala(tonica, tonalidade).values()
+    notas, graus = _escala(tonica, tonalidade).values()
+
+    for grau in graus:
+        table.add_column(grau)
+    table.add_row(*notas)
+    console.print(table)
+
+
+@app.command()
+def acorde(
+    cifra: str = Argument('c', help='Cifra desejada'),
+):
+    table = Table()
+    notas, graus = _acorde(cifra).values()
 
     for grau in graus:
         table.add_column(grau)
